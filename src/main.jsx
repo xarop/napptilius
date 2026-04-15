1f23b7386a97e1f1f7bf2bdc1cdb8b77fb6252dc
xarop <ajl@xarop.com>
2026-04-15 18:23:54 +0200

    feat: add GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/napptilius/">
       <App />
     </BrowserRouter>
   </StrictMode>
